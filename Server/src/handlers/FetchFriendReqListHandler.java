be7ef93048de8e737e599a9cd1926c9dbe425c5f
abhi-biswas <mpsabhijeet@gmail.com>
2019-12-16 01:48:03 +0530

    Added Result class for FetchFriendReq

--- Server/src/handlers/FetchFriendReqListHandler.java
+++ Server/src/handlers/FetchFriendReqListHandler.java
@@ -1,7 +1,9 @@
 package handlers;
 
+import constants.BinaryStatus;
 import mainclasses.Connector;
 import requests.FetchFriendReqListRequest;
+import results.FetchFriendReqListResult;
 import results.ShortUserDetail;
 
 import java.sql.PreparedStatement;
@@ -11,22 +13,28 @@ import java.util.ArrayList;
 
 public class FetchFriendReqListHandler {
     FetchFriendReqListRequest req;
-
     public FetchFriendReqListHandler(FetchFriendReqListRequest req) {
         this.req = req;
     }
-    public ArrayList handle() throws SQLException {
+    public FetchFriendReqListResult handle() {
         ArrayList<ShortUserDetail> list = new ArrayList<>();
         String query =
                 "Select A.username,A.fname,A.lname,A.city  from user A, `friendrequests` B where B.recievername = ? and B.sendername = A.username" ;
-        PreparedStatement statement = Connector.getConnection().prepareStatement(query);
-        statement.setString(1,req.getUsername());
-        ResultSet set = statement.executeQuery();
-        while(set.next())
-        {
-            list.add(new ShortUserDetail(set.getString(1),set.getString(2),set.getString(3),set.getString(4)));
+        try {
+            PreparedStatement statement = Connector.getConnection().prepareStatement(query);
+            statement.setString(1,req.getUsername());
+            ResultSet set = statement.executeQuery();
+            while(set.next())
+            {
+                list.add(new ShortUserDetail(set.getString(1),set.getString(2),set.getString(3),set.getString(4)));
+            }
         }
-        return list;
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            return new FetchFriendReqListResult(BinaryStatus.FAILURE);
+        }
+        return new FetchFriendReqListResult(list);
     }
 
 }
---
+++ Server/src/handlers/SendFriendRequestHandler.java
@@ -0,0 +1,40 @@
+package handlers;
+import constants.BinaryStatus;
+import mainclasses.Connector;
+import requests.SendFriendRequest;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.Calendar;
+
+public class SendFriendRequestHandler {
+
+   private SendFriendRequest req;
+   Connection con ;
+
+    public SendFriendRequestHandler(SendFriendRequest req) {
+        this.req = req;
+        con = Connector.getConnection();
+    }
+    public BinaryStatus handle()  {
+        String query = "insert into `friendrequests` values (?,?,?)";
+        try {
+            PreparedStatement statement = con.prepareStatement(query);
+            Calendar calendar = Calendar.getInstance();
+            Timestamp timestamp = new Timestamp(calendar.getTime().getTime());
+            statement.setString(1,req.getSenderName());
+            statement.setString(2,req.getReceiverName());
+            statement.setTimestamp(3,timestamp);
+            int n = statement.executeUpdate();
+            if(n==1)
+                return BinaryStatus.SUCCESS;
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return BinaryStatus.FAILURE;
+    }
+}
