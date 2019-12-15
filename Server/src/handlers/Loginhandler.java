7a8bc99bd31f449c79d8906c1fd4f97b8318acd9
KUNAL ANAND <anandkunal241@gmail.com>
2019-12-15 17:28:27 +0530

    loginhandler updated

--- Server/src/handlers/Loginhandler.java
+++ Server/src/handlers/Loginhandler.java
@@ -27,10 +27,11 @@ public class Loginhandler {
     public Loginresult checkCredentials() throws Exception{
 
 
-        connection = Connector.connecttoDatabase("Sakshatkar");
+        connection = Connector.getConnection();
 
-        String query = "select * from user where username = '"+ this.loginrequest.getUsername() + "'";
+        String query = "select * from user where username = ?";
         PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setString(1, this.loginrequest.getUsername());
         ResultSet resultSet = preparedStatement.executeQuery();
         if(resultSet.next()){
                 if(loginrequest.getPassword().equals(resultSet.getString("password")))
@@ -48,19 +49,28 @@ public class Loginhandler {
         switch (loginstatus) {
 
             case SUCCESS:
-                String query = "select * from user where username = '" + this.loginrequest.getUsername() + "'";
+                String query = "select * from user where username = ?";
                 PreparedStatement preparedStatement = connection.prepareStatement(query);
+                preparedStatement.setString(1,this.loginrequest.getUsername());
                 ResultSet resultSet = preparedStatement.executeQuery();
                 Loginresult loginresult = null;
+
+
                 if(resultSet.next()) {
                     loginresult = new Loginresult(loginstatus, this.loginrequest.getUsername(),
                             resultSet.getString("fname"), resultSet.getString("lname"));
 
-                    query = "insert into login values('" + this.loginrequest.getUsername() + "', '" + this.ip + "', false)";
-                    System.out.println(query);
+                    query = "insert into `login` (username, ip, status) values(?, ?, ?)";
+
                     preparedStatement = connection.prepareStatement(query);
+                    preparedStatement.setString(1,this.loginrequest.getUsername());
+                    preparedStatement.setString(2, this.ip);
+                    preparedStatement.setBoolean(3, false);
+
                     preparedStatement.executeUpdate();
                 }
+
+
                 return loginresult;
             case USERNOTEXIST:
 
