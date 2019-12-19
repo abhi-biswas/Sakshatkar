d8fc32b042db547e773666413113fc15a87c8105
abhi-biswas <mpsabhijeet@gmail.com>
2019-12-19 19:53:13 +0530

    Added SendMessage Handler
    Signed-off-by: abhi-biswas <mpsabhijeet@gmail.com>

--- Server/src/results/ImageMessage.java
+++ Server/src/results/ImageMessage.java
@@ -5,14 +5,14 @@ import constants.MessageType;
 import java.sql.Timestamp;
 
 public class ImageMessage extends Message {
-    private Byte imagedata[];
+    private byte imagedata[];
 
-    public ImageMessage(MessageType type, String sender, String receiver, Timestamp timestamp, Byte[] imagedata) {
-        super(type, sender, receiver, timestamp);
+    public ImageMessage( String sender, String receiver, Timestamp timestamp, byte[] imagedata) {
+        super(MessageType.IMAGE, sender, receiver, timestamp);
         this.imagedata = imagedata;
     }
 
-    public Byte[] getImagedata() {
+    public byte[] getImagedata() {
         return imagedata;
     }
 }
--- Server/src/results/TextMessage.java
+++ Server/src/results/TextMessage.java
@@ -7,8 +7,8 @@ import java.sql.Timestamp;
 public class TextMessage extends Message {
     String content;
 
-    public TextMessage(MessageType type, String sender, String receiver, Timestamp timestamp, String content) {
-        super(type, sender, receiver, timestamp);
+    public TextMessage( String sender, String receiver, Timestamp timestamp, String content) {
+        super(MessageType.TEXT, sender, receiver, timestamp);
         this.content = content;
     }
 
